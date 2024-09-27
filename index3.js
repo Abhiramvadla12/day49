2a936f16ad22bbda376073509d404afcce225340
Abhiramvadla12 <abhiramvadla61@gmail.com>
2024-09-27 06:22:21 +0530

    version 2

--- index3.js
+++ index3.js
@@ -1,7 +1,7 @@
 //named functiion:
 const numbers = [4, 10, 15, 6, 25, 8];
 function lessThan10(value){
-    return value < 10;
+    return value >= 10;
 }
 function squares(element){
     return element*element;
@@ -10,12 +10,12 @@ let resultOfFilter = numbers.filter(lessThan10);
 let resultOfMap = resultOfFilter.map(squares);
 console.log(resultOfMap);
 //anonymous function:
-resultOfFilter = numbers.filter(function(value){ return value < 10;
+resultOfFilter = numbers.filter(function(value){ return value >= 10;
 });
 resultOfMap = resultOfFilter.map(function(element){ return element*element});
 console.log(resultOfMap)
 //arrow function:
-resultOfFilter = numbers.filter((value)=> { return value < 10;
+resultOfFilter = numbers.filter((value)=> { return value >= 10;
 });
 resultOfMap = resultOfFilter.map((element) => { return element*element});
 console.log(resultOfMap);
